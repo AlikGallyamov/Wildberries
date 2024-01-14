74ed235665aca19e8486db11a426f2f3042a533e
Alik Gallyamov <alik.gallyamov2202@gmail.com>
2024-01-14 18:25:34 +0500

    added 3 tests

--- wildberries_tests/data/data_cards.py
+++ wildberries_tests/data/data_cards.py
@@ -4,7 +4,19 @@ import dataclasses
 @dataclasses.dataclass
 class Card:
     card_name: str
+    brand_name: str
     card_article: str
+    non_existent_article: str
 
 
-card = Card(card_name='Машинка игрушка коллекционная металлическая Волшебное Королевство', card_article='150179366')
+card = Card(
+    card_name='Машинки игрушки для мальчиков набор Молния Маквин Тачки Cars',
+    brand_name='MITU',
+    card_article='76566619',
+    non_existent_article='8944165849321186418794'
+)
+
+@dataclasses.dataclass
+class Texts:
+    nothing_found: str = 'Попробуйте поискать по-другому или сократить запрос'
+    empty_cart: str = 'В корзине пока пусто'
--- wildberries_tests/pages/main_page.py
+++ wildberries_tests/pages/main_page.py
@@ -1,4 +1,5 @@
-from selene import browser, be, have
+from selene import browser, be, have, query, command
+from wildberries_tests.data.data_cards import Texts
 
 import time
 
@@ -6,16 +7,40 @@ import time
 class MainPage:
     def __init__(self):
         self.search_input = browser.element('#searchInput')
+        self.basket = browser.element('[class*=item-basket]')
 
     def open_url(self):
         browser.open('/')
-        time.sleep(3)
+        browser.element('[class="general-preloader"]').should(be.not_.visible)
+
 
     def search(self, value):
         self.search_input.type(value).press_enter()
 
-    def result_by_name(self, card_name):
-        browser.element('[data-nm-id="150179366"] a').should(have.attribute('aria-label', card_name))
+    def result_by_name(self, card_name, brand_name, card_article):
+        browser.element(f'[data-nm-id="{card_article}"] a').should(have.attribute('aria-label', f'{card_name} {brand_name}'))
 
     def result_by_article(self, card_article):
         browser.element('#productNmId').should(have.text(card_article))
+
+    def no_result(self):
+        browser.element('.not-found-search__text').should(have.text(Texts.nothing_found))
+
+    def add_item_to_cart(self, card_article):
+
+        browser.element(f'[data-nm-id="{card_article}"]').hover()
+        browser.element(f'[data-nm-id="{card_article}"] [class*="card__add"]').click()
+
+    def open_basket(self):
+        self.basket.click()
+    def check_cart(self, card_name, brand_name):
+        self.open_basket()
+        browser.all('[class="good-info__title j-product-popup"] span').should(have.texts(card_name, brand_name))
+
+    def remove_from_cart(self):
+        self.open_basket()
+        browser.element('[class*=item-del]').click()
+        browser.element('[class*="basket-empty__title"]').should(have.text(Texts.empty_cart))
+
+
+
