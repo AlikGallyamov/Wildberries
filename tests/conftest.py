9048333c7d2b2ef8ea4848366f1826d6f719d534
Alik Gallyamov <alik.gallyamov2202@gmail.com>
2024-01-18 00:47:05 +0500

    Added choice of remote or local launch

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import os
 from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from pydantic_settings import BaseSettings
 
 from wildberries_tests.controls import attach
 from dotenv import load_dotenv
@@ -13,14 +14,19 @@ def load_env():
     load_dotenv()
 
 
+class Settings(BaseSettings):
+    window_width: str = '1600'
+    window_height: str = '950'
+    base_url: str = 'https://www.wildberries.ru'
+    remote: bool = True
+
+
 @pytest.fixture(autouse=True)
 def browser_settings():
+    config = Settings()
     options = Options()
-    browser.config.window_width = 1600
-    browser.config.window_height = 960
-    browser.config.base_url = 'https://www.wildberries.ru'
+
     driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'normal'
     selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "100.0",
@@ -29,16 +35,19 @@ def browser_settings():
             "enableVideo": True
         }
     }
-    login = os.getenv('LOGIN')
-    password = os.getenv('PASSWORD')
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-        options=options)
+    if config.remote:
+        login = os.getenv('LOGIN')
+        password = os.getenv('PASSWORD')
+        options.capabilities.update(selenoid_capabilities)
+        driver = webdriver.Remote(
+            command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+            options=options)
+        browser.config.driver = driver
 
-    browser.config.driver = driver
     browser.config.driver_options = driver_options
-
+    browser.config.base_url = config.base_url
+    browser.config.window_width = config.window_width
+    browser.config.window_height = config.window_height
     yield
 
     attach.add_html(browser)
